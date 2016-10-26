28217bc1d21c59b724d9841ce2a1fe5c3f89cfa9
Ninja-kitten <tran260@mail.chapman.edu>
2016-10-25 22:12:11 -0700

    Update Window.h
    added decrementWindow method

--- Window.h
+++ Window.h
@@ -10,7 +10,8 @@ public:
     ~Window();
     bool empty() const;
     void occupy(const Student stu);
-    bool timeup();
+    bool timeup(); //tells if the student's time is up
+    void decrementWindow(); //decrements student window time
     void remove();
     unsigned int getIdle() const;
     void increaseIdle();
