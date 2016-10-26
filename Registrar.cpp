2402a4c40e3b6d68818ae3af955037256e8b546b
Ninja-kitten <tran260@mail.chapman.edu>
2016-10-25 21:24:52 -0700

    Update Registrar.cpp

--- Registrar.cpp
+++ Registrar.cpp
@@ -28,21 +28,30 @@ Registrar::~Registrar(){
 	}
 }
 
-void Registrar::increaseWait(){
+void Registrar::increaseWait(){//goes through the line, increments everyones wait time
 	GenQueue<Student>* templine = new GenQueue<Student>();
 	Student tempStud;
 	while(line -> getSize() != 0)
-	{	tempStud = line->peek();
+	{	
+		tempStud = line->peek();
 	 	tempStud.increaseWait();
 		templine->enqueue(tempStud);
 		line->dequeue();
 	}
 	line = templine;
+	//switch pointers
 	templine = NULL;
 	delete templine;
 }
 
-void Registrar::increaseIdle(){}
+void Registrar::increaseIdle() // goes through windows and increases idle time if empty
+{
+	for(int i = 0; i<size; ++i){
+		if(windows[i].empty()){
+			windows[i].increaseIdle();
+		}
+	}
+}
 
 void Registrar::setWindow(const unsigned int& n)
 {
@@ -72,8 +81,29 @@ bool Registrar::openWindow()
     return false;
 }
 
-void Registrar::occupyWindow(){}
+void Registrar::occupyWindow(){
+	for(int i = 0; i < size; ++i)
+	{
+		if(windows[i].empty())
+		{
+			windows[i].occupy(line->peek())
+			waitTimes.addBack(line->peek()->getWait());
+			//add student timew to wait times list
+			line->dequeue;
+			//fills empty windows with students
+			windowTimes.addBack(windows[i].getIdle())
+			//adds idletimes to window times list
+			windowTimes.resetIdle();
+		}
+	}
+}
 
-void Registrar::emptyWindow(){}
+void Registrar::emptyWindow(){//need to add code!!!
+	for(int i=0; i < size; ++i) //goes through every window
+	{
+		if(windows[i])//if student is out of window time
+			window[i].remove();
+	}
+}
 
 #endif
