d23c5a1a8a77814559646d075670629eb4cf7f0a
Ninja-kitten <tran260@mail.chapman.edu>
2016-10-26 17:00:02 -0700

    Update StatStuff.h

--- StatStuff.h
+++ StatStuff.h
@@ -1,10 +1,17 @@
 class StatStuff{
 private:
   GenQueue<int>* data;
+  double mean;
+  double median;
+  double var;
+  double min;
+  double max
+
 public:
   StatStuff();
   StatStuff(GenQueue<int>* d);
   ~StatStuff();
+  doubleSetData();
   getMean();
   getMedian();
   getVar();
