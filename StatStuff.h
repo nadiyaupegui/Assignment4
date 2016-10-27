0307747e828ac889c6f2b215789d16c3a690c818
Ninja-kitten <tran260@mail.chapman.edu>
2016-10-26 17:07:58 -0700

    Update StatStuff.h

--- StatStuff.h
+++ StatStuff.h
@@ -5,21 +5,24 @@ private:
   double median;
   double var;
   double min;
-  double max
+  double max;
+  int overVal;
 
 public:
   StatStuff();
   StatStuff(GenQueue<int>* d);
   ~StatStuff();
-  doubleSetData();
-  getMean();
-  getMedian();
-  getVar();
-  getMin();
-  getMax();
-  calcMean();
-  calcMedian();
-  calcVar();
-  calcMin();
-  calcMax();
+  double SetData();
+  double getMean();
+  double getMedian();
+  double getVar();
+  double getMin();
+  double getMax();
+  int getOverVal();
+  void calcMean();
+  void calcMedian();
+  void calcVar();
+  void calcMin();
+  void calcMax();
+  void countOverVal(int val);
 }
